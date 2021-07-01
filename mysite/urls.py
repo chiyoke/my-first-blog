fb7b679ad712522710439e4234c4b24c6991ee4f
chiyoke <86583645+chiyoke@users.noreply.github.com>
2021-07-01 23:39:03 +0800

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
